9a922aa3ca5441e255c10e0445b71bc63aad4a9d
darwishdev <a.darwish.dev@gmail.com>
2025-06-11 18:06:01 +0300

    ignore vitepress cache from docs

--- apps/docs/docs/.vitepress/cache/deps/primevue_dialogservice.js
+++
@@ -1,9 +0,0 @@
-import {
-  DialogService
-} from "./chunk-UWJ6LGRX.js";
-import "./chunk-K5A633JT.js";
-import "./chunk-GQV6R5GZ.js";
-import "./chunk-4MBMRILA.js";
-export {
-  DialogService as default
-};
--- apps/docs/docs/.vitepress/cache/deps/primevue_toastservice.js
+++
@@ -1,9 +0,0 @@
-import {
-  ToastService
-} from "./chunk-F7M6KFBQ.js";
-import "./chunk-K5A633JT.js";
-import "./chunk-GQV6R5GZ.js";
-import "./chunk-4MBMRILA.js";
-export {
-  ToastService as default
-};
